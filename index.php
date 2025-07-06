de3b1bad24e949081dd49e78a3dc02ee373cc51c
Olando McDonald <olandomcdonald07@gmail.com>
2025-07-06 01:17:07 -0500

    Add login and signup pages with new styles
    Introduced main.css for unified styling and replaced styles.css. Added main.php and signup.php as new pages. Updated index.php to provide a styled login form and navigation between login and signup pages.

--- index.php
+++ index.php
@@ -1,16 +1,39 @@
 <?php
 
 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Job Postings: Government of Manitoba</title>
+    <link rel="stylesheet" href="main.css">
+    <title>Job Postings: Login</title>
 </head>
 <body>
-    
+    <div class="main-overlay">
+        <div class="input">
+            <form action="index.php" method="POST">
+                <h1>Login</h1>
+                <div class="input-div">
+                    <div class="input-fields">
+                        <label for="username">Username:</label>
+                        <input type="text" name="username" id="username" placeholder="username">
+                    </div>
+                    <div class="input-fields">
+                        <label for="password">Password:</label>
+                        <input type="text" name="password" id="password" placeholder="password">
+                    </div>
+                    <div class="input-btns">
+                        <button><a href="index.php">Login</a></button>
+                        <button><a href="signup.php">Sign Up</a></button>
+                        <!-- <button>Login</button>
+                        <button>Sign Up</button> -->
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
---
+++ signup.php
@@ -0,0 +1,28 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Job Postings: Sign Up</title>
+</head>
+<body>
+    <div class='main-overlay'>
+        <div class='input'>
+            <form action="index.php" method="POST">
+                <h1>Sign Up</h1>
+                <div class="input-div">
+                    <div class="input-btns">
+                        <button><a href="index.php">Login</a></button>
+                        <button><a href="signup.php">Sign Up</a></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
