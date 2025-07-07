32a8453566a035aa85a52d92bb243275a79723c5
Olando McDonald <olandomcdonald07@gmail.com>
2025-07-06 21:05:06 -0500

    Enhance login and signup forms with new fields and UI
    Added additional input fields to the signup form, including first name, last name, age, address, education, and skills. Improved password input with show/hide functionality on both login and signup forms. Updated CSS for better layout and support for new form elements.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+$password = $_POST["username"];
 
 ?>
 <!DOCTYPE html>
@@ -19,21 +20,33 @@
                 <div class="input-div">
                     <div class="input-fields">
                         <label for="username">Username:</label>
-                        <input type="text" name="username" id="username" placeholder="username">
+                        <input type="text" name="username" id="username" placeholder="Username">
                     </div>
                     <div class="input-fields">
                         <label for="password">Password:</label>
-                        <input type="text" name="password" id="password" placeholder="password">
+                        <input type="password" name="password" id="password" placeholder="Password">
+                        <div class="password-div">
+                            <input type="checkbox" onclick="showPassword()"><p>Show Password</p>
+                        </div>
                     </div>
                     <div class="input-btns">
                         <button><a href="index.php">Login</a></button>
                         <button><a href="signup.php">Sign Up</a></button>
-                        <!-- <button>Login</button>
-                        <button>Sign Up</button> -->
                     </div>
                 </div>
             </form>
         </div>
     </div>
+
+    <script>
+        function showPassword(){
+            var password = document.getElementById("password");   
+            if(password.type === "password"){
+                password.type = "text";
+            }else{
+                password.type = "password";
+            }     
+        }
+    </script>
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -16,6 +16,59 @@
             <form action="index.php" method="POST">
                 <h1>Sign Up</h1>
                 <div class="input-div">
+                    <div class="input-fields">
+                        <label for="first_name">First Name:</label>
+                        <input type="text" name="first_name" id="first_name" placeholder="First name">
+                    </div>
+                    <div class="input-fields">
+                        <label for="last_name">Last Name:</label>
+                        <input type="text" name="last_name" id="last_name" placeholder="Last name">
+                    </div>
+                    <div class="input-fields">
+                        <label for="age">Select your age:</label>
+                        <select name="age" id="age">
+                            <option value="placeholder" selected>Click to select</option>
+                            <option value="option">16-20</option>
+                            <option value="option">21-30</option>
+                            <option value="option">31-40</option>
+                            <option value="option">41-50</option>
+                            <option value="option">51-60</option>
+                            <option value="option">61-70</option>                            
+                        </select>
+                    </div>
+                    <div class="input-fields">
+                        <label for="address">Address:</label>
+                        <input type="text" name="address" id="address" placeholder="Address">
+                    </div>
+                    <div class="input-fields">
+                        <label for="education">Select your education level:</label>
+                        <select name="education" id="education">
+                            <option value="placeholder" selected>Click to select</option>
+                            <option value="option">No Certificate, Diploma or Degree</option>
+                            <option value="option">High School Diploma</option>
+                            <option value="option">College/University</option>
+                            <option value="option">Certificate/Diploma</option>
+                            <option value="option">Bachelor's Degree</option>
+                            <option value="option">Master's Degree</option>
+                            <option value="option">Doctoral Degree</option>
+                            <option value="option">Professional Degree</option>
+                        </select>
+                    </div>
+                    <div class="input-fields">
+                        <label for="Skills">List your skills:</label>
+                        <textarea name="Skills" id="Skills" placeholder="Skills"></textarea>
+                    </div>
+                    <div class="input-fields">
+                        <label for="username">Username:</label>
+                        <input type="text" name="username" id="username" placeholder="Username">
+                    </div>
+                    <div class="input-fields">
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" id="password" placeholder="Password">
+                        <div class="password-div">
+                            <input type="checkbox" onclick="showPassword()"><p>Show Password</p>
+                        </div>
+                    </div>
                     <div class="input-btns">
                         <button><a href="index.php">Login</a></button>
                         <button><a href="signup.php">Sign Up</a></button>
@@ -24,5 +77,16 @@
             </form>
         </div>
     </div>
+
+    <script>
+        function showPassword(){
+            var password = document.getElementById("password");   
+            if(password.type === "password"){
+                password.type = "text";
+            }else{
+                password.type = "password";
+            }     
+        }
+    </script>
 </body>
 </html>
